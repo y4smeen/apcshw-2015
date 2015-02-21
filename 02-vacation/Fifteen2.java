6fc607963190b9e332e7d2236db689ba346a1af3
Yasmeen Roumie <y4smeen@gmail.com>
2015-02-21 00:35:52 -0500

    fix formatting

--- 02-vacation/Fifteen2.java
+++ 02-vacation/Fifteen2.java
@@ -3,31 +3,39 @@ import java.util.*;
 
 public class Fifteen2 {
 
-  private int[][] board; // actual game board
+  private String[][] board; // actual game board
   private int dim; // board dimension
+  private int size = dim * dim; // total number of spaces on board
+  private Random random = new Random();
 
   public Fifteen2() {
     dim = 4;
-    board = new int[dim][dim];
+    board = new String[dim][dim];
     int num = 1;
     for (int y = 0; y < dim; y++) {
       for (int x = 0; x < dim; x++) {
-        if (num < 16) {
-          board[x][y] = num;
+        if (num < 10) {
+          board[x][y] = "0" + Integer.toString(num);
           num++;
-        } else {
-          board[x][y] = 0;
+        }
+        else if (num > 9 && num < 16) {
+          board[x][y] = Integer.toString(num);
+          num++;
+        }
+        else {
+          board[x][y] = "00";
         }
       }
     }
   }
 
   public String toString() {
-    String s = "[2J\n";
+    // String s = "[2J\n";
+    String s = "\n";
     int count = 1;
     for (int y = 0; y < dim; y++) {
       for (int x = 0; x < dim; x++) {
-        s += board[x][y];
+        s += board[x][y] + " ";
         if (count % 4 == 0) {
           s += "\n";
         }
@@ -37,8 +45,22 @@ public class Fifteen2 {
     return s;
   }
 
+  public void shuffle() {
+    for (int i = size; i > 1; i--) {
+        exchange(i - 1, random.nextInt(i));
+    }
+  }
+
+  public void exchange(int x, int y) {
+    String temp = board[x / dim][x % dim];
+    board[x / dim][x % dim] = board[y / dim][y % dim];
+    board[y / dim][y % dim] = temp;
+  }
+
   public static void main(String[] args) {
     Fifteen2 f = new Fifteen2();
+    System.out.println(f);
+    f.shuffle();
     System.out.println(f);
   }
 
