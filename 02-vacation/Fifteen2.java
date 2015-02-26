5d7e80f9131729b23dcebec375e16d4ed1111b5c
Yasmeen Roumie <y4smeen@gmail.com>
2015-02-25 20:47:16 -0500

    make smarter shuffle method

--- 02-vacation/Fifteen2.java
+++ 02-vacation/Fifteen2.java
@@ -63,6 +63,39 @@ public class Fifteen2 {
         board[j][i] = numList.get(j + i * dim);
       }
     }
+    System.out.println("SHUFFLED!");
+  }
+
+  public void smartShuffle() {
+    Random randShuffle = new Random();
+    Random tile = new Random();
+    for (int i = 0; i < 100; i++){
+      String coord = getCoord(board, tile.nextInt(16));
+      int x = Integer.parseInt(coord.substring(0, 1));
+      int y = Integer.parseInt(coord.substring(1));
+      int z = moveLimit(coord);
+      int move = randShuffle.nextInt(4);
+
+      // System.out.println("Move limit: " + z);
+      // System.out.println("Coordinate: " + x + ", " + y);
+
+      // 0: move up
+      if (move == 0 && z != 2 && z != 8 && z != 10) {
+        swap(x, y, x, y - 1);
+      }
+      // 1: move down
+      else if (move == 1 && z % 3 != 0) {
+        swap(x, y, x, y + 1);
+      }
+      // 2: move right
+      else if (move == 2 && z % 4 != 0) {
+        swap(x, y, x + 1, y);
+      }
+      // 3: move left
+      else if (move == 3 && z % 5 != 0) {
+        swap(x, y, x - 1, y);
+      }
+    }
   }
 
   // x1, y1 is swapped with x2, y2
@@ -70,6 +103,36 @@ public class Fifteen2 {
     String hold = board[x2][y2];
     board[x2][y2] = board[x1][y1];
     board[x1][y1] = hold;
+  }
+
+  public int moveLimit(String coord) {
+    int x = Integer.parseInt(coord.substring(0, 1));
+    int y = Integer.parseInt(coord.substring(1));
+    int limit = 1;
+    int special = 1;
+    if (dim > 1) {
+      // 1: top restriction
+      if (y == 0) {
+        limit = 2; //2,8,10
+      }
+      // 2: bottom restriction
+      if (y == dim - 1) {
+        limit = 3; // 3,12,15
+      }
+      // 3: right restriction
+      if (x == dim - 1) {
+        special = 4; // 4,8,12
+      }
+      // 4: left restriction
+      if (x == 0) {
+        special = 5; // 5,10,15
+      }
+      // 5:
+    }
+
+    // System.out.println("in moveLimit: " + limit + "*" + special);
+    return limit*special;
+
   }
 
   // Checks if two spots are next to each other
@@ -85,10 +148,6 @@ public class Fifteen2 {
     return "false";
   }
 
-  public boolean isSolvable() {
-    return false;
-  }
-
   // Calculates number of steps to get to a certain spot
   public int manhattanDist(String coord1, String coord2) {
     int x1 = Integer.parseInt(coord1.substring(0, 1));
@@ -97,6 +156,17 @@ public class Fifteen2 {
     int y2 = Integer.parseInt(coord2.substring(1));
 
     return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+  }
+
+  public int totalDist() {
+    int sum = 0;
+    String coord1, coord2;
+    for (int i = 0; i < size; i++) {
+      coord1 = getCoord(origin, i);
+      coord2 = getCoord(board, i);
+      sum += manhattanDist(coord1, coord2);
+    }
+    return sum;
   }
 
   // Heuristically solves the 15 Puzzle
@@ -118,11 +188,12 @@ public class Fifteen2 {
 
   public static void main(String[] args) {
     Fifteen2 f = new Fifteen2();
+    System.out.println("Original Board");
     System.out.println(f);
-    f.shuffle();
-    //System.out.println(f.isNeighbor("01", "03"));
+    System.out.println("Shuffled Board");
+    f.smartShuffle();
     System.out.println(f);
-    // System.out.println(f.getCoord(board, 16));
+
 
   }
 
