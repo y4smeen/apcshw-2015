4abd3cd7d912094979152b199a119020cfdfa491
Yasmeen Roumie <y4smeen@gmail.com>
2015-03-08 23:42:04 -0400

    add stuff

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -14,7 +14,31 @@ public class Select {
   }
 
   public int Select(int[] A, int k, int l, int h) {
-    
+    int p = A[k];
+    int Li = l
+    int Hi = h - 1;
+    int Pi;
+
+    while (Li != Hi) {
+      if(A[Li] < p) {
+        Li++;
+      }
+      else {
+        swap(A, A[Li], A[Hi]);
+        Hi--;
+      }
+    }
+
+    if (p > )
+
+
+  }
+
+  public int[] swap(int[] A, int i1, int i2) {
+    int hold = A[i1];
+    A[i1] = A[i2];
+    A[i2] = hold;
+    return A;
   }
 
 }
