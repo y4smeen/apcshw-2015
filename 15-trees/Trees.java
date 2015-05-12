6d3e7df7e0e5c315aae857e3962ba6e83b982cbb
Yasmeen Roumie <y4smeen@gmail.com>
2015-05-12 12:03:25 -0400

    asdfad

--- 15-trees/Trees.java
+++ 15-trees/Trees.java
@@ -13,8 +13,7 @@ public class Trees {
       return 0;
     }
     else {
-      return 1 + height(maxValue(T.getLeft())) +
-      height(maxValue(T.getRight()));
+      return 1 + Math.max(height(T.getLeft()), height(T.getRight()));
     }
   }
 
@@ -22,11 +21,52 @@ public class Trees {
 
   }
 
-  // diameter: longest leaf-to-leaf path
-  public int longest(Node T) {
-
+  /* HEAP
+  1. binary tree
+  2. parent is smaller than either child
+  3. as full as possible left to right
+  */
+  public longest(Node T) {
+    if (T == null) {
+      return 0;
+    }
+    int root = 1 + height(T.getLeft()) + height(T.getRight());
+    int left = longest(T.getLeft());
+    int right = longest(T.getRight());
+    return Math.max(root, Math.max(left, right));
   }
 
+  /* diameter: longest leaf-to-leaf path
+  public int diameter(Node T) {
+    if (T == null) {
+      return 0;
+    }
+    else {
+      PL1 = height(T.getLeft())+
+        height(T.getRight()) +
+        2;
+      PL2 = diameter(T.getLeft());
+      PL3 = diameter(T.getRight());
+      return max(PL1, PL2, PL3);
+    }
+  }
+  */
+  /* Remove min
+  1. pull off root
+  2. move lowest rightest value to root
+  3. pushdown(root)
+      while node > either child
+        swap w/smaller child
+        and work down tree
+  removemin/pushdown --> O(log n)
+  heapsort --> O(n log n)
+  findmin() --> O(1)
+  insert --> O(log n)
+  create --> O(n log n) (really it's O(n))
+
+  given node n, n's children are at 2n, 2n+1
+  parent int(n/2)
+  */
   public numNodes(Node T) {
     if (T == null) {
       return 0;
